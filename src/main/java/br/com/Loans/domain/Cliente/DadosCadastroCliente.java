bd992ded46ac0b2fa62e910b96084f0694d65384
MuriloPizolito <murilopizolito@gmail.com>
2025-02-07 00:06:48 -0300

    validations

--- src/main/java/br/com/Loans/domain/Cliente/DadosCadastroCliente.java
+++ src/main/java/br/com/Loans/domain/Cliente/DadosCadastroCliente.java
@@ -1,24 +1,30 @@
 package br.com.Loans.domain.Cliente;
 
-import br.com.Loans.domain.Loan.LoansType;
+import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.PositiveOrZero;
+import org.hibernate.validator.constraints.br.CPF;
 
 public record DadosCadastroCliente(
 
-        @NotNull
+        @NotNull(message = "Idade é obrigatória")
+        @Min(value = 18, message = "Idade mínima de 18 anos")
         int idade,
 
-        @NotBlank
+
+        @NotBlank(message = "CPF é obrigatório")
+        @CPF(message = "CPF inválido")
         String cpf,
 
-        @NotBlank
+        @NotBlank(message = "Nome é obrigatório")
         String nome,
 
-        @NotNull
+        @NotNull(message = "Renda é obrigatória")
+        @PositiveOrZero(message = "Renda não pode ser negativa")
         double renda,
 
-        @NotBlank
+        @NotBlank(message = "Localização é obrigatória")
         String localizacao
         ) {
 }
